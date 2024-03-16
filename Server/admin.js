6ff18004f9a11fda9f77f7f05e839e878933136e
Abhijith P A <65763546+abhinlr@users.noreply.github.com>
2024-03-16 11:53:07 +0530

    Update admin.js

--- Server/admin.js
+++ Server/admin.js
@@ -7,7 +7,7 @@ const authRoute = require('./routes/auth')
 const app = express();
 password = '';
 
-const db = 'mongodb+srv://tastebuds:tastebuds@cluster0.htvoug9.mongodb.net/TasteBuds?retryWrites=true&w=majority&appName=Cluster0'
+const db = 'mongodb+srv://tastebuds:......@cluster0.htvoug9.mongodb.net/TasteBuds?retryWrites=true&w=majority&appName=Cluster0'
 
 
 mongoose.connect(db).then(() => console.log('Database connection successful')).catch(err => console.error('MongoDB connection error:', err));
